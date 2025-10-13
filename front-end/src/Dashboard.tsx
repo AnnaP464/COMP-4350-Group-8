34a2bd56ecf58ee6747c8ab7cd447afc5ff3f6a1
AnnaP464 <sholokh1@myumanitoba.ca>
2025-10-13 12:43:55 -0500

    just an empty dashboard idk how to style this baby

--- front-end/src/Dashboard.tsx
+++ front-end/src/Dashboard.tsx
@@ -5,15 +5,6 @@ const Dashboard: React.FC = () => {
   return (
     <div style={{ padding: "2rem" }}>
       <h1>Welcome to your Dashboard 🎉</h1>
-      <p>You’re now logged in.</p>
-      <div style={{ marginTop: "2rem" }}>
-        <button>
-            View Profile
-        </button>
-        <button style={{ marginLeft: "1rem" }}>
-            Logout
-        </button>
-      </div>
     </div>
   );
 };
